6658fd02218922cf55fc067f708f5745a25eed1b
ADinushika <amalidinushika93@gmail.com>
2018-07-30 10:41:58 +0530

    #1 Create index.php page

---
+++ index.php
@@ -0,0 +1,307 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="icon" type="image/png" href="assets/images/favicon.png">
+
+        <title>CasonsTaxi</title>
+
+        <link href="assets/css/bootstrap-grid.css" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet">
+        <link href="assets/css/swiper.css" rel="stylesheet">
+        <link href="assets/css/swipebox.css" rel="stylesheet">
+        <link href="assets/css/zoomslider.css" rel="stylesheet">
+        <link href="assets/css/style2.css" rel="stylesheet">
+        <link href="assets/css/styles.css" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+
+        <link href="https://fonts.googleapis.com/css?family=Fira+Sans+Condensed:700,800%7COpen+Sans:400,600,700" rel="stylesheet"> 
+
+        <script type="text/javascript" src="assets/js/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>
+
+        <!-- Header -->
+        <?php
+        include 'header.php';
+        ?>
+        <!-- /Header -->
+
+        <!-- Slider -->
+        <?php
+        include 'slider.php';
+        ?>
+        <!-- /Slider -->
+
+        <div class="homepage-block-yellow-2">
+        </div>
+
+        <section id="car-block">
+            <div class="car-right animation-block">
+                <img src="assets/images/_car-big-side.png" alt="Car"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-7">
+                        <h4 class="yellow"></h4>
+                        <h2 class="h1">Welcome</h2>
+                    </div>
+                    <div class="col-md-6">
+                        <p class="para-align btn-car-block">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet.</p>
+
+                        <!--                        <ul class="check two-col strong">
+                                                    <li>Luxury cars</li>
+                                                    <li>No fee</li>
+                                                    <li>Weekly payment</li>
+                                                    <li>Fixed price</li>
+                                                    <li>Good application</li>
+                                                    <li>Stable orders</li>
+                                                </ul>-->
+                        <a href="#" class="btn btn-yellow btn-lg btn-white">Read More</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="tariffs" class="parallax" style="background-image: url(assets/images/_tariff-bg.jpg);">
+            <div class="container">
+                <h4 class="yellow">See Our</h4>
+                <h2 class="h1">Vehicles</h2>
+                <div class="row">
+                    <div class="col-md-3 col-sm-6">
+                        <div class="item matchHeight">
+                            <div class="image"><img src="assets/images/_tariff-1.png" class="full-width" alt="Tariff"></div>
+                            <h4>Standart</h4>
+                            <p>Standard sedan for a drive around the city at your service</p>
+                            <div class="price">$2<span>/km</span></div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="item matchHeight">
+                            <div class="image"><img src="assets/images/_tariff-2.png" class="full-width" alt="Tariff"></div>
+                            <h4>Business</h4>
+                            <p>Standard sedan for a drive around the city at your service</p>
+                            <div class="price">$2,7<span>/km</span></div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="item matchHeight vip">
+                            <div class="image"><img src="assets/images/_tariff-3.png" class="full-width" alt="Tariff"></div>
+                            <h4 class="red">Vip</h4>
+                            <p>Standard sedan for a drive around the city at your service</p>
+                            <div class="price">$5<span>/km</span></div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="item matchHeight">
+                            <div class="image"><img src="assets/images/_tariff-4.png" class="full-width" alt="Tariff"></div>
+                            <h4>Bus-minivan</h4>
+                            <p>Standard sedan for a drive around the city at your service</p>
+                            <div class="price">$4,5<span>/km</span></div>
+                        </div>
+                    </div>												
+                </div>
+            </div>
+        </section>
+
+        <section id="services">
+            <div class="container">
+                <h4 class="yellow">Welcome</h4>
+                <h2 class="h1">Our Services</h2>
+                <div class="row">
+                    <div class="col-md-3 col-sm-6 col-ms-6 matchHeight">	
+                        <div class="image"><img src="assets/images/_services-1.png" alt="Service"></div>
+                        <h5>Rapid city transfer</h5>
+                        <p>We will bring you quickly and comfortably to anywhere in your city</p>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-ms-6 matchHeight">	
+                        <div class="image"><img src="assets/images/_services-2.png" alt="Service"></div>
+                        <h5>Booking a hotel</h5>
+                        <p>If you need a comfortable hotel, our operators will book it for you, and take a taxi to the address</p>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-ms-6 matchHeight">	
+                        <div class="image"><img src="assets/images/_services-3.png" alt="Service"></div>
+                        <h5>Airport transfer</h5>
+                        <p>We will bring you quickly and comfortably to anywhere in your city</p>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-ms-6 matchHeight">	
+                        <div class="image"><img src="assets/images/_services-4.png" alt="Service"></div>
+                        <h5>Baggage transport</h5>
+                        <p>If you need a comfortable hotel, our operators will book it for you, and take a taxi to the address</p>
+                    </div>
+                </div>
+            </div>
+        </section>		
+
+        <section id="download" class="parallax" style="background-image: url(assets/images/_download-bg.jpg);">
+            <div class="container">
+                <h4 class="yellow">Get More Benefits</h4>
+                <h2 class="h1">Download The App</h2>
+                <div class="row">
+                    <div class="col-md-4 col-sm-12">
+                        <div class="items row">
+                            <div class="col-md-2 visible-md visible-lg"><span class="num">01.</span></div>
+                            <div class="col-md-10">
+                                <h5 class="yellow">Fast booking</h5>
+                                <p>Nam ac ligula congue, interdum enim sit amet, fermentum nisi.</p>
+                            </div>
+                            <div class="col-md-2 visible-md visible-lg"><span class="num">02.</span></div>
+                            <div class="col-md-10">
+                                <h5 class="yellow">Easy to use</h5>
+                                <p>Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                            </div>						
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-md-push-4 col-sm-12">
+                        <div class="items items-right row">
+                            <div class="col-md-10">
+                                <h5 class="yellow">GPS searching</h5>
+                                <p>Ut elementum tincidunt erat vel ornare. Suspendisse ac felis non diam pretium.</p>
+                            </div>
+                            <div class="col-md-2 visible-md visible-lg"><span class="num">03.</span></div>
+                            <div class="col-md-10">
+                                <h5 class="yellow">Bonuses for ride</h5>
+                                <p>Phasellus l et porta tortor dignissim at. Pellentesque gravida tortor at euismod mollis. </p>
+                            </div>						
+                            <div class="col-md-2 visible-md visible-lg"><span class="num">04.</span></div>
+                        </div>				
+                    </div>				
+                    <div class="col-md-4 col-md-pull-4 col-sm-12">
+                        <div class="mob">
+                            <a href="#"><img src="assets/images/_app-google.png" alt="App"></a>
+                            <a href="#"><img src="assets/images/_app-apple.png" alt="App"></a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>			
+
+        <section id="testimonials">
+            <hr class="lg">
+            <div class="container">
+                <h4 class="yellow">Happy Client's</h4>
+                <h2 class="h1">Testimonials</h2>
+
+                <div class="swiper-container row" id="testimonials-slider">
+                    <div class="swiper-wrapper">
+                        <div class="col-md-4 col-sm-6 swiper-slide">
+                            <div class="inner matchHeight">
+                                <div class="text">
+                                    <p>Nullam orci dui, dictum et magna sollicitudin,  a id lacus. Fusce tincidunt in leo lacinia condimentum.</p>
+                                </div>
+                                <div class="quote">
+                                    <span class="fa fa-quote-left"></span>
+                                    <div class="name">Anastasia Stone</div>
+                                    <img src="assets/images/_client-1.jpg" alt="Client">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-sm-6 swiper-slide">
+                            <div class="inner matchHeight">
+                                <div class="text">
+                                    <p>Suspendisse nec arcu sed nibh lacinia pretium. Phasellus eros ligula, mattis id rutrum non, eleifend vitae lacus. </p>
+                                </div>				
+                                <div class="quote">		
+                                    <span class="fa fa-quote-left"></span>
+                                    <div class="name">Steven Rashford</div>
+                                    <img src="assets/images/_client-4.jpg" alt="Client">
+                                </div>
+                            </div>
+                        </div>	
+                        <div class="col-md-4 col-sm-6 swiper-slide">
+                            <div class="inner matchHeight">
+                                <div class="text">
+                                    <p>Quisque sollicitudin feugiat risus, eu posuere ex euismod eu. Phasellus hendrerit, massa efficitur dapibus pulvinar, sapien eros sodales ante, euismod aliquet nulla metus a mauris. </p>
+                                </div>			
+                                <div class="quote">			
+                                    <span class="fa fa-quote-left"></span>
+                                    <div class="name">Patrick James</div>
+                                    <img src="assets/images/_client-5.jpg" alt="Client">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-sm-6 swiper-slide">
+                            <div class="inner matchHeight">
+                                <div class="text">
+                                    <p>Nullam orci dui, dictum et magna sollicitudin, tempor blandit erat. Maecenas suscipit tellus sit amet augue placerat fringilla a id lacus. Fusce tincidunt in leo lacinia condimentum.</p>
+                                </div>
+                                <div class="quote">
+                                    <span class="fa fa-quote-left"></span>
+                                    <div class="name">Anastasia Stone</div>
+                                    <img src="assets/images/_client-1.jpg" alt="Client">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-sm-6 swiper-slide">
+                            <div class="inner matchHeight">
+                                <div class="text">
+                                    <p>Suspendisse nec arcu sed nibh lacinia pretium. Phasellus eros ligula, mattis id rutrum non, eleifend vitae lacus. </p>
+                                </div>				
+                                <div class="quote">		
+                                    <span class="fa fa-quote-left"></span>
+                                    <div class="name">Steven Rashford</div>
+                                    <img src="assets/images/_client-4.jpg" alt="Client">
+                                </div>
+                            </div>
+                        </div>	
+                        <div class="col-md-4 col-sm-6 swiper-slide">
+                            <div class="inner matchHeight">
+                                <div class="text">
+                                    <p>Quisque sollicitudin feugiat risus, eu posuere ex euismod eu. Phasellus hendrerit, massa efficitur dapibus pulvinar, sapien eros sodales ante, euismod aliquet nulla metus a mauris. </p>
+                                </div>			
+                                <div class="quote">			
+                                    <span class="fa fa-quote-left"></span>
+                                    <div class="name">Patrick James</div>
+                                    <img src="assets/images/_client-5.jpg" alt="Client">
+                                </div>
+                            </div>
+                        </div>					
+                    </div>
+                    <div class="arrows">
+                        <a href="#" class="arrow-left fa fa-caret-left"></a>
+                        <a href="#" class="arrow-right fa fa-caret-right"></a>
+                    </div>				
+                </div>
+            </div>
+        </section>
+
+        <!-- Footer -->
+        <?php
+        include 'footer.php';
+        ?>
+        <!-- /Footer -->
+
+        <script>var base_href = '/';</script>
+        <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>	
+
+        <script type="text/javascript" src="assets/js/plugins.min.js"></script>		
+
+        <script src="assets/js/map-style.js"></script>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCTRSHf8sjMCfK9PHPJxjJkwrCIo5asIzE"></script>	
+        <script type="text/javascript" src="assets/js/scripts.js"></script>
+
+        <script>
+            (function (i, s, o, g, r, a, m) {
+                i['GoogleAnalyticsObject'] = r;
+                i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+                a = s.createElement(o),
+                        m = s.getElementsByTagName(o)[0];
+                a.async = 1;
+                a.src = g;
+                m.parentNode.insertBefore(a, m)
+            })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+            ga('create', 'UA-91006724-1', 'auto');
+            ga('send', 'pageview');
+        </script>
+
+    </body>
+</html>
